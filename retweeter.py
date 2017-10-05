f0a0e4a6b03ef31f979244fab48cf28aeedd8db7
omar <omar.verduga@gmail.com>
2017-10-06 00:10:07 +0100

    'v2'

--- retweeter.py
+++ retweeter.py
@@ -5,7 +5,7 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-for tweet in tweepy.Cursor(api.search, q='qmul math').items():
+for tweet in tweepy.Cursor(api.search, q='Cioran').items():
     try:
         print('\nTweet by: @' + tweet.user.screen_name)
 
@@ -14,14 +14,14 @@ for tweet in tweepy.Cursor(api.search, q='qmul math').items():
         print('Retweeted the tweet')
 
         # Favorite the tweet
-        #tweet.favorite()
+        tweet.favorite()
         print('Favorited the tweet')
 
         # Follow the user who tweeted
-        # tweet.user.follow()
+        tweet.user.follow()
         print('Followed the user')
 
-        sleep(1)
+        sleep(15)
 
     except tweepy.TweepError as e:
         print(e.reason)
